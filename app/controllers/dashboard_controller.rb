35782b942a144b2d48765d52d25255e88b1b3c15
Jon Lemmon <jonlemmon@gmail.com>
2013-03-28 15:01:48 +1300

    WIP

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,5 @@ class DashboardController < BaseController
   def show
     @discussions_with_current_motion_voted_on = current_user.discussions_with_current_motion_voted_on
     @discussions_with_current_motion_not_voted_on = current_user.discussions_with_current_motion_not_voted_on
-    @discussion = Discussion.new
   end
 end
